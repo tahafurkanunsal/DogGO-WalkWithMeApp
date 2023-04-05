8680c562ed4cdee52cbcdd0e89ce2a5a3ac19d4e
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-04-05 15:37:16 +0300

    initial commit

---
+++ src/main/java/com/tahafurkan/sandbox/Walk/With/Me/App/WalkWithMeApp.java
@@ -0,0 +1,13 @@
+package com.tahafurkan.sandbox.Walk.With.Me.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkWithMeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkWithMeApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/tahafurkan/sandbox/Walk/With/Me/App/WalkWithMeAppTests.java
@@ -0,0 +1,13 @@
+package com.tahafurkan.sandbox.Walk.With.Me.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkWithMeAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
